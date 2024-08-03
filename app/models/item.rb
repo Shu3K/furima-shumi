9d2045a5be6a4cea9fdf3bc4da70181e17bb386e
Shu3K <shumi.kaufman@gmail.com>
2024-08-03 19:41:19 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,11 @@ class Item < ApplicationRecord
   belongs_to :payment
   belongs_to :prefecture
   belongs_to :day
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :payment_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :day_id, numericality: { other_than: 1 , message: "can't be blank" } 
+
 end
