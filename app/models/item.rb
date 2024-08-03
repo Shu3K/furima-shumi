55651f6aa9baa7c011c4325b44cb896314b32d15
Shu3K <shumi.kaufman@gmail.com>
2024-08-03 19:26:49 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :payment
+  belongs_to :prefecture
+  belongs_to :day
 end
