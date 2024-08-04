eaeb6c4741fa7e1dbf1e2b4050f9ddb1a0a1e881
Shu3K <shumi.kaufman@gmail.com>
2024-08-04 16:20:46 +0900

    出品データの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,14 @@ class ItemsController < ApplicationController
 
   def create
     @items = Item.new(item_params)
+    if @items.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:category_id, :condition_id, :prefecture_id, :payment_id, :day_id, :image, :name, :explain, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,15 @@ class Item < ApplicationRecord
   belongs_to :payment
   belongs_to :prefecture
   belongs_to :day
+  has_one_attached :image
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :payment_id, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :day_id, numericality: { other_than: 1 , message: "can't be blank" } 
-
+  validates :image, presence: true 
+  validates :name, presence: true 
+  validates :explain, presence: true 
+  validates :price, presence: true,  numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is invalid"}
 end
