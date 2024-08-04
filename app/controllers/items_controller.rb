7542db4dbbe50b9b0f5c769271d503d32e62c0bf
Shu3K <shumi.kaufman@gmail.com>
2024-08-04 15:02:13 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,13 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
   def index
   end
 
   def new
-    @items = item.new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.new(item_params)
   end
 end
